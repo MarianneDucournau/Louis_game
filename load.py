5a5608dde09530e7c27799f7d91891edd2b421c0
Marianne Borzic <marianne.borzic@uber.com>
2018-11-28 12:46:53 -0800

    Add question

--- load.py
+++ load.py
@@ -1,7 +1,7 @@
 from params import *
 from Tkinter import *
 from PIL import Image, ImageTk
-from random import randint
+from random import sample
 import os
 
 
@@ -39,12 +39,33 @@ def open_image(name_img_file):
     photo = ImageTk.PhotoImage(image)
     return photo
 
-def generate_list_photos(list_images, n=2):
+
+def generate_list_random_int(list_images, n=2):
     """"
-    Generate a list of tk photos of length n based on the list of images names
+    Generate a list of unique random int of length n based on the length of list of images names
     """
 
-    list_photos = [open_image(list_images[randint(0, len(list_images) - 1)]) for i in range(n)]
+    assert (n <= len(list_images))
+
+    list_random_int = sample(range(0, len(list_images) - 1), n)
+    return list_random_int
+
+
+def generate_one_random_int(list_random_int):
+    """"
+    Generate a random int from list_random_int
+    """
+
+    one_random_int = sample(list_random_int, 1)
+    return one_random_int
+
+
+def generate_list_photos(list_images, list_random_int):
+    """"
+    Generate the list of tk photos based on the list of images names and the list of random int
+    """
+
+    list_photos = [open_image(list_images[i]) for i in list_random_int]
     return list_photos
 
 
@@ -59,12 +80,27 @@ def display_images(panned_window, list_photos):
     return panned_window
 
 
+def display_question(panned_window, one_int, list_images):
+    """"
+    Display question in panned_window
+    """
+
+    panned_window.add(Label(panned_window, text='Montre la couleur {}'.format(list_images[one_int[0]]), anchor=CENTER))
+    panned_window.pack()
+    return panned_window
+
+
 def main():
-    p = create_window()
-    out = list_images()
-    print out
-    list_photos = generate_list_photos(list_images=out)
-    panned_window = display_images(p, list_photos)
+    window = create_window()
+    image_names = list_images()
+    print image_names
+    random_int = generate_list_random_int(list_images=image_names)
+    print(random_int)
+    photos = generate_list_photos(list_images=image_names, list_random_int=random_int)
+    panned_window = display_images(window, photos)
+    one_int = generate_one_random_int(random_int)
+    panned_window = display_question(panned_window, one_int, image_names)
+    print(image_names[one_int[0]])
     mainloop()
 
 
